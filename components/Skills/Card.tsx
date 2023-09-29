28e75bfdc3c00c9337b229c95501e442b76e6d8d
JC Mujica <jcmujica@gmail.com>
2023-09-29 16:49:15 -0300

    fixit

--- components/Skills/Card.tsx
+++ components/Skills/Card.tsx
@@ -2,35 +2,35 @@ import React, { useState, useEffect } from 'react'
 import styles from 'styles/Skills.module.css'
 
 interface ICardProps {
-    name: string;
-    src: string;
-    reset: number;
+  name: string
+  src: string
+  reset: number
 }
 
 
 export const Card = (props: ICardProps) => {
-    const [clicked, setClicked] = useState(false);
-    const { name = '', src = '/', reset } = props;
+  const [clicked, setClicked] = useState(false)
+  const { name = '', src = '/', reset } = props
 
-    useEffect(() => {
-        setClicked(false);
-    }, [reset]);
+  useEffect(() => {
+    setClicked(false)
+  }, [reset])
 
-    const handleClick = () => {
-        setClicked(!clicked);
-    };
+  const handleClick = () => {
+    setClicked(!clicked)
+  }
 
-    return (
-        <div
-            className={
-                !clicked ?
-                    `${styles.card}` :
-                    `${styles.card} ${styles.card__animated}`
-            }
-            onClick={handleClick}
-        >
-            <h3 className={styles.card__name}>{name}</h3>
-            <img src={src} alt={name} className={styles.card__img} />
-        </div>
-    )
+  return (
+    <div
+      className={
+        !clicked ?
+          `${styles.card}` :
+          `${styles.card} ${styles.card__animated}`
+      }
+      onClick={handleClick}
+    >
+      <h3 className={styles.card__name}>{name}</h3>
+      <img src={src} alt={name} className={styles.card__img} />
+    </div>
+  )
 }
--- components/Skills/Skills.tsx
+++ components/Skills/Skills.tsx
@@ -1,51 +1,51 @@
 import React, { useState, useEffect } from 'react'
 import styles from 'styles/Skills.module.css'
-import { Card } from './Card';
+import { Card } from './Card'
 import skillsConstant from 'constants/skills.json'
 
 interface ISkill {
-    name: string;
-    src: string;
+  name: string
+  src: string
 }
 
 export const Skills = () => {
-    const [skills, setSkills] = useState<ISkill[] | []>([]);
-    const [resetValue, setResetValue] = useState(0);
+  const [skills, setSkills] = useState<ISkill[] | []>([])
+  const [resetValue, setResetValue] = useState(0)
 
-    useEffect(() => {
-        setSkills(skillsConstant);
-    }, []);
+  useEffect(() => {
+    setSkills(skillsConstant)
+  }, [])
 
-    const handleRandomize = () => {
-        const newSkills = [...skills];
-        newSkills.sort(() => Math.random() - 0.5);
-        handleReset();
-        setSkills(newSkills);
-    };
+  const handleRandomize = () => {
+    const newSkills = [...skills]
+    newSkills.sort(() => Math.random() - 0.5)
+    handleReset()
+    setSkills(newSkills)
+  }
 
-    const handleReset = () => {
-        setResetValue(resetValue + 1);
-    };
+  const handleReset = () => {
+    setResetValue(resetValue + 1)
+  }
 
-    return (
-        <section className={styles.skills} id='skills'>
-            <h2>My skills</h2>
-            <div className={styles.skills__list}>
-                {skills.map(skill => (
-                    <Card
-                        key={skill.name}
-                        name={skill.name}
-                        src={skill.src}
-                        reset={resetValue}
-                    />
-                ))}
-            </div>
-            <button
-                onClick={handleRandomize}
-                className={styles.skills__button}
-            >
-                Randomize 🎲
-            </button>
-        </section>
-    )
-};
+  return (
+    <section className={styles.skills} id='skills'>
+      <h2>My skills</h2>
+      <div className={styles.skills__list}>
+        {skills.map(skill => (
+          <Card
+            key={skill.name}
+            name={skill.name}
+            src={skill.src}
+            reset={resetValue}
+          />
+        ))}
+      </div>
+      <button
+        onClick={handleRandomize}
+        className={styles.skills__button}
+      >
+        Randomize 🎲
+      </button>
+    </section>
+  )
+}
