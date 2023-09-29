19903e3a7da7c1c167349e17c72e37ec6de4db68
JC Mujica <jcmujica@gmail.com>
2023-09-29 17:10:02 -0300

    change text

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,29 +4,29 @@ import contactLinks from 'constants/contact.json'
 
 export const Footer = () => {
 
-    return (
-        <footer className={styles.footer}>
-            <div className={styles.footer__right}>
-                <div className={styles.footer__links}>
-                    {contactLinks.map((link, i) => (
-                        <div key={`footer-${i}`} className={styles.footer__item}>
-                            <a href={link.url}
-                                className={styles.footer__link}
-                            >
-                                <img src={link.img}
-                                    alt={link.name}
-                                    className={styles.footer__img}
-                                    width='40px'
-                                    height='40px'
-                                />
-                                <span>{link.name}</span>
-                            </a>
-                        </div>
-                    ))}
-                </div>
-                <p className={styles.footer__text}>{`Copyright JC Mujica \u00A9 ${new Date().getFullYear()}. All rights reserved.`}</p>
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.footer__right}>
+        <div className={styles.footer__links}>
+          {contactLinks.map((link, i) => (
+            <div key={`footer-${i}`} className={styles.footer__item}>
+              <a href={link.url}
+                className={styles.footer__link}
+              >
+                <img src={link.img}
+                  alt={link.name}
+                  className={styles.footer__img}
+                  width='40px'
+                  height='40px'
+                />
+                <span>{link.name}</span>
+              </a>
             </div>
-            <img src="/assets/jc-blue.png" alt="JC Logo" className={styles.footer__logo} />
-        </footer>
-    )
-};
+          ))}
+        </div>
+        <p className={styles.footer__text}>{`Copyright JC Mujica \u00A9 ${new Date().getFullYear()}. All rights reserved.`}</p>
+      </div>
+      <img src="/assets/jc-blue.png" alt="JC Logo" className={styles.footer__logo} />
+    </footer>
+  )
+}
